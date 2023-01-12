b5de78f222e2f863c86c41108fcb90f881f5acbb
José Bruno de Oliveira <jbfo@cin.ufpe.br>
2023-01-12 15:16:50 -0300

    refactor: Create a function to trigger bot execution

--- index.py
+++ index.py
@@ -2,21 +2,25 @@ from rewards.rewards import Rewards
 
 import time
 
-try:
-  with Rewards() as bot:
+def run_bot():
+  try:
+    with Rewards() as bot:
 
-    try:
-      bot.navigate_to_page('https://rewards.bing.com/')
-      bot.find_available_tasks()
+      try:
+        bot.navigate_to_page('https://rewards.bing.com/')
+        bot.find_available_tasks()
 
-      time.sleep(5)
+        time.sleep(5)
 
-      bot.search_on_bing()
+        bot.search_on_bing()
 
-    except Exception as e:
-      raise
+      except Exception as e:
+        raise
 
-    bot.close()
+      bot.close()
 
-except Exception as e:
-  raise
+  except Exception as e:
+    raise
+
+if __name__ == "__main__":
+  run_bot()
