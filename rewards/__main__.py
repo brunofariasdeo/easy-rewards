b0844e2844ba6961d6df976c2a9f507f74b04ff5
José Bruno de Oliveira <jose.oliveira@encoura.org>
2023-02-09 22:00:17 -0300

    refactor: Create main file

---
+++ rewards/__main__.py
@@ -0,0 +1,40 @@
+import logging
+from rewards.rewards import Rewards
+import time
+
+def run_bot():
+  try:
+    with Rewards() as bot:
+
+      try:
+        bot.navigate_to_page('https://rewards.bing.com/')
+        bot.find_available_tasks()
+
+        time.sleep(5)
+
+        bot.search_on_bing()
+
+        time.sleep(5)
+
+        bot.play_a_game("Fortnite")
+
+      except Exception as e:
+        raise
+
+      bot.close()
+
+  except Exception as e:
+    raise
+
+def setup_logging():
+  logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s:%(message)s'
+  )
+
+if __name__ == "__main__":
+  setup_logging()
+
+  logging.info("Bot started")
+  run_bot()
+  logging.info("Bot finished")
