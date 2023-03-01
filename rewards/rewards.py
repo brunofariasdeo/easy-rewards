092f2c0b7c8c5ae7d0878d7eaa55b36f2319d97f
José Bruno de Oliveira <jose.oliveira@encoura.org>
2023-02-28 23:07:03 -0300

    fix: Change the way that the headless option is triggered

--- rewards/rewards.py
+++ rewards/rewards.py
@@ -17,8 +17,9 @@ class Rewards(webdriver.Edge):
     self.pointsToRedeem = True
     self.tasksToClick = True
     options = Options()
-
-    options.add_argument("headless" if headless else "None")
+    # The argument "--headless" is currently not working due to a Chrome Webdriver bug,
+    # as per https://github.com/SeleniumHQ/selenium/issues/11634
+    options.add_argument("headless=new" if headless else "None")
     options.add_argument("--mute-audio")
     options.add_argument(f'user-data-dir={constants.PROFILE_PATH}')
     options.add_argument(f'profile-directory={constants.PROFILE_NAME}')
